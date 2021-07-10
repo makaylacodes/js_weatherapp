c0a48b79395a44b84abacca72fcb76d34e919d9a
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-07-11 00:04:38 +0900

    I have added a function that will change the weather from celsius to fahrenheit, if the user clicks on the screen

--- app.js
+++ app.js
@@ -67,11 +67,38 @@ function getWeather(latitude, longitude){
 
         }
         );
+    }
+    function displayWeather(){
+        iconElement.innerHTML = `<img src = "icons/${weather.iconId}.jpg"/>`;
+        tempElement.innerHTML = `${weather.temperature.value}<span>C</span>`;
+        descElement.innerHTML = weather.description;
+        locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+    }
 
-        function displayWeather(){
-            iconElement.innerHTML = `<img src = "icons/${weather.iconId}.jpg"/>`;
+    // C to F
+    function celsiusToFahrenheit(temperature){
+        return (temperature * 9/5) +32;
+
+    }
+
+    //When user clocks on the temp
+
+    tempElement.addEventListener("click", function(){
+        if(weather.temperature.value === undefined) return;
+
+        if(weather.temperature.unit == "celsius"){
+            let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+            fahrenheit = Math.floor(fahrenheit);
+
+            tempElement.innerHTML = `${fahrenheit}<span>F</span>`;
+            weather.temperature.unit = "fahrenheit";
+        
+        }else{
             tempElement.innerHTML = `${weather.temperature.value}<span>C</span>`;
-            descElement.innerHTML = weather.description;
-            locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+            weather.temperature.unit = "celsius";
         }
-}
+
+    });
+
+
+    
